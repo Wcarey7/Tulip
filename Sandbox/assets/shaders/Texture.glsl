d36ed56166ca2a9dd9578d4ab50108de5bb2b0cb
Wcarey7 <wadecarey7@gmail.com>
2023-07-05 09:25:49 -0700

    SetFloat function in Shaders. Cleaned up Texture.glsl

--- Sandbox/assets/shaders/Texture.glsl
+++ Sandbox/assets/shaders/Texture.glsl
@@ -8,13 +8,11 @@ uniform mat4 u_ViewProjection;
 uniform mat4 u_Transform;
 
 out vec2 v_TexCoord;
-out vec2 v_ScreenPos;
 
 void main()
 {
     v_TexCoord = a_TexCoord;
     gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
-    v_ScreenPos = gl_Position.xy;
 }
 
 #type fragment
@@ -23,15 +21,12 @@ void main()
 layout(location = 0) out vec4 color;
 
 in vec2 v_TexCoord;
-in vec2 v_ScreenPos;
 
 uniform vec4 u_Color;
+uniform float u_TilingFactor;
 uniform sampler2D u_Texture;
 
 void main()
 {
-    float dist = 1.0f - distance(v_ScreenPos * 0.8f, vec2(0.0f));
-    dist = clamp(dist, 0.0f, 1.0f);
-    dist = sqrt(dist);
-    color = texture(u_Texture, v_TexCoord) * u_Color * dist;
+    color = texture(u_Texture, v_TexCoord * u_TilingFactor) * u_Color;
 }
