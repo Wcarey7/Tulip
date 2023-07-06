92a375c47f648f3b47c985b81feb909e09e09b93
Wcarey7 <wadecarey7@gmail.com>
2023-07-06 11:26:53 -0700

    Instrumentor fixes

--- Tulip/src/Tulip/Debug/Instrumentor.h
+++ Tulip/src/Tulip/Debug/Instrumentor.h
@@ -32,8 +32,10 @@ namespace Tulip
     struct ProfileResult
     {
         std::string Name;
-        long long Start, End;
-        uint32_t ThreadID;
+
+        FloatingPointMicroseconds Start;
+        std::chrono::microseconds ElapsedTime;
+        std::thread::id ThreadID;
     };
 
     struct InstrumentationSession
@@ -44,55 +46,86 @@ namespace Tulip
     class Instrumentor
     {
     private:
+        std::mutex m_Mutex;
         InstrumentationSession* m_CurrentSession;
         std::ofstream m_OutputStream;
-        int m_ProfileCount;
     public:
         Instrumentor()
-            : m_CurrentSession(nullptr), m_ProfileCount(0)
+            : m_CurrentSession(nullptr)
         {
         }
 
         void BeginSession(const std::string& name, const std::string& filepath = "results.json")
         {
+            std::lock_guard lock(m_Mutex);
+            if (m_CurrentSession)
+            {
+                // If there is already a current session, then close it before beginning new one.
+                // Subsequent profiling output meant for the original session will end up in the
+                // newly opened session instead.  That's better than having badly formatted
+                // profiling output.
+                if (Log::GetCoreLogger()) // Edge case: BeginSession() might be before Log::Init()
+                {
+                    TULIP_CORE_ERROR("Instrumentor::BeginSession('{0}') when session '{1}' already open.", name, m_CurrentSession->Name);
+                }
+                InternalEndSession();
+            }
             m_OutputStream.open(filepath);
-            WriteHeader();
-            m_CurrentSession = new InstrumentationSession{ name };
+
+            if (m_OutputStream.is_open())
+            {
+                m_CurrentSession = new InstrumentationSession({name});
+                WriteHeader();
+            }
+            else
+            {
+                if (Log::GetCoreLogger()) // Edge case: BeginSession() might be before Log::Init()
+                {
+                    TULIP_CORE_ERROR("Instrumentor could not open results file '{0}'.", filepath);
+                }
+            }
         }
 
         void EndSession()
         {
-            WriteFooter();
-            m_OutputStream.close();
-            delete m_CurrentSession;
-            m_CurrentSession = nullptr;
-            m_ProfileCount = 0;
+            std::lock_guard lock(m_Mutex);
+            InternalEndSession();
         }
 
         void WriteProfile(const ProfileResult& result)
         {
-            if (m_ProfileCount++ > 0)
-                m_OutputStream << ",";
+            std::stringstream json;
 
-            std::string name = result.Name;
-            std::replace(name.begin(), name.end(), '"', '\'');
+            json << std::setprecision(3) << std::fixed;
+            json << ",{";
+            json << "\"cat\":\"function\",";
+            json << "\"dur\":" << (result.ElapsedTime.count()) << ',';
+            json << "\"name\":\"" << result.Name << "\",";
+            json << "\"ph\":\"X\",";
+            json << "\"pid\":0,";
+            json << "\"tid\":" << result.ThreadID << ",";
+            json << "\"ts\":" << result.Start.count();
+            json << "}";
 
-            m_OutputStream << "{";
-            m_OutputStream << "\"cat\":\"function\",";
-            m_OutputStream << "\"dur\":" << (result.End - result.Start) << ',';
-            m_OutputStream << "\"name\":\"" << name << "\",";
-            m_OutputStream << "\"ph\":\"X\",";
-            m_OutputStream << "\"pid\":0,";
-            m_OutputStream << "\"tid\":" << result.ThreadID << ",";
-            m_OutputStream << "\"ts\":" << result.Start;
-            m_OutputStream << "}";
-
-            m_OutputStream.flush();
+            std::lock_guard lock(m_Mutex);
+            if (m_CurrentSession)
+            {
+                m_OutputStream << json.str();
+                m_OutputStream.flush();
+            }
         }
+
+        static Instrumentor& Get()
+        {
+            static Instrumentor instance;
+            return instance;
+        }
+
+    private:
 
         void WriteHeader()
         {
-            m_OutputStream << "{\"otherData\": {},\"traceEvents\":[";
+            m_OutputStream << "{\"otherData\": {},\"traceEvents\":[{}";
             m_OutputStream.flush();
         }
 
@@ -102,11 +135,19 @@ namespace Tulip
             m_OutputStream.flush();
         }
 
-        static Instrumentor& Get()
+        // Note: you must already own lock on m_Mutex before
+        // calling InternalEndSession()
+        void InternalEndSession()
         {
-            static Instrumentor instance;
-            return instance;
+            if (m_CurrentSession)
+            {
+                WriteFooter();
+                m_OutputStream.close();
+                delete m_CurrentSession;
+                m_CurrentSession = nullptr;
+            }
         }
+
     };
 
     class InstrumentationTimer
@@ -115,7 +156,7 @@ namespace Tulip
         InstrumentationTimer(const char* name)
             : m_Name(name), m_Stopped(false)
         {
-            m_StartTimepoint = std::chrono::high_resolution_clock::now();
+            m_StartTimepoint = std::chrono::steady_clock::now();
         }
 
         ~InstrumentationTimer()
@@ -126,28 +167,57 @@ namespace Tulip
 
         void Stop()
         {
-            auto endTimepoint = std::chrono::high_resolution_clock::now();
+            auto endTimepoint = std::chrono::steady_clock::now();
+            auto highResStart = FloatingPointMicroseconds{ m_StartTimepoint.time_since_epoch() };
+            auto elapsedTime = std::chrono::time_point_cast<std::chrono::microseconds>(endTimepoint).time_since_epoch() - std::chrono::time_point_cast<std::chrono::microseconds>(m_StartTimepoint).time_since_epoch();
 
-            long long start = std::chrono::time_point_cast<std::chrono::microseconds>(m_StartTimepoint).time_since_epoch().count();
-            long long end = std::chrono::time_point_cast<std::chrono::microseconds>(endTimepoint).time_since_epoch().count();
-
-            uint32_t threadID = std::hash<std::thread::id>{}(std::this_thread::get_id());
-            Instrumentor::Get().WriteProfile({ m_Name, start, end, threadID });
+            Instrumentor::Get().WriteProfile({ m_Name, highResStart, elapsedTime, std::this_thread::get_id() });
 
             m_Stopped = true;
         }
     private:
         const char* m_Name;
-        std::chrono::time_point<std::chrono::high_resolution_clock> m_StartTimepoint;
+        std::chrono::time_point<std::chrono::steady_clock> m_StartTimepoint;
         bool m_Stopped;
     };
+
+    namespace InstrumentorUtils {
+
+        template <size_t N>
+        struct ChangeResult
+        {
+            char Data[N];
+        };
+
+        template <size_t N, size_t K>
+        constexpr auto CleanupOutputString(const char(&expr)[N], const char(&remove)[K])
+        {
+            ChangeResult<N> result = {};
+
+            size_t srcIndex = 0;
+            size_t dstIndex = 0;
+            while (srcIndex < N)
+            {
+                size_t matchIndex = 0;
+                while (matchIndex < K - 1 && srcIndex + matchIndex < N - 1 && expr[srcIndex + matchIndex] == remove[matchIndex])
+                    matchIndex++;
+                if (matchIndex == K - 1)
+                    srcIndex += matchIndex;
+                result.Data[dstIndex++] = expr[srcIndex] == '"' ? '\'' : expr[srcIndex];
+                srcIndex++;
+            }
+            return result;
+        }
+    }
 }
 
 #define TULIP_PROFILE 0
 #if TULIP_PROFILE
     #define TULIP_PROFILE_BEGIN_SESSION(name, filepath) ::Tulip::Instrumentor::Get().BeginSession(name, filepath)
     #define TULIP_PROFILE_END_SESSION() ::Tulip::Instrumentor::Get().EndSession()
-    #define TULIP_PROFILE_SCOPE(name) ::Tulip::InstrumentationTimer timer##__LINE__(name);
+    #define TULIP_PROFILE_SCOPE(name) constexpr auto fixedName = ::Tulip::InstrumentorUtils::CleanupOutputString(name, "__cdecl ");\
+                                    ::Tulip::InstrumentationTimer timer##__LINE__(fixedName.Data)
+
     #define TULIP_PROFILE_FUNCTION() TULIP_PROFILE_SCOPE(__FUNCSIG__)
 #else
     #define TULIP_PROFILE_BEGIN_SESSION(name, filepath)
