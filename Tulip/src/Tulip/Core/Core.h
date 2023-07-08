0170d1197e59d5d06d9f8b414d38efe208bbc2a6
Wcarey7 <wadecarey7@gmail.com>
2023-07-08 08:52:28 -0700

    std:bind Event macro switched to a Lambda

--- Tulip/src/Tulip/Core/Core.h
+++ Tulip/src/Tulip/Core/Core.h
@@ -32,7 +32,7 @@
 
 #define BIT(x) (1 << x)
 
-#define TULIP_BIND_EVENT_FN(fn) std::bind(&fn, this, std::placeholders::_1)
+#define TULIP_BIND_EVENT_FN(fn) [this](auto&&... args)->decltype(auto) { return this->fn(std::forward<decltype(args)>(args)...); }
 
 //----- Smart Pointer Macros ------------------------------
 //---------------------------------------------------------
