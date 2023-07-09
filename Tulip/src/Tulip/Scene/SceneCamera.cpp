44e7f0537f0462c2cfa862a3848ea9b04bcfd37a
Wcarey7 <wadecarey7@gmail.com>
2023-07-08 21:49:36 -0700

    Fixed inconsistent variable names in camera GUI

--- Tulip/src/Tulip/Scene/SceneCamera.cpp
+++ Tulip/src/Tulip/Scene/SceneCamera.cpp
@@ -11,19 +11,20 @@ namespace Tulip
         RecalculateProjection();
     }
 
-    void SceneCamera::SetOrthographic(float size, float nearClip, float farClip)
-    {
-        m_OrthographicSize = size;
-        m_OrthographicNear = nearClip;
-        m_OrthographicFar = farClip;
-        RecalculateProjection();
-    }
     void SceneCamera::SetPerspective(float verticalFOV, float nearClip, float farClip)
     {
         m_ProjectionType = ProjectionType::Perspective;
         m_PerspectiveFOV = verticalFOV;
         m_PerspectiveNear = nearClip;
         m_PerspectiveFar = farClip;
+        RecalculateProjection();
+    }
+
+    void SceneCamera::SetOrthographic(float size, float nearClip, float farClip)
+    {
+        m_OrthographicSize = size;
+        m_OrthographicNear = nearClip;
+        m_OrthographicFar = farClip;
         RecalculateProjection();
     }
 
