8c0d059c53ef848421961140eb8a6ae8c7bbe1ec
Wcarey7 <wadecarey7@gmail.com>
2023-07-09 12:32:04 -0700

    UI to add/remove components

--- Tulip/src/Tulip/Scene/Scene.cpp
+++ Tulip/src/Tulip/Scene/Scene.cpp
@@ -29,6 +29,11 @@ namespace Tulip
         return entity;
     }
 
+    void Scene::DestroyEntity(Entity entity)
+    {
+        m_Registry.destroy(entity);
+    }
+
     void Scene::OnUpdate(Timestep ts)
     {
         // Update scripts. 'nsc' is NativeScriptComponent
@@ -94,4 +99,37 @@ namespace Tulip
         }
 
     }
+
+    template<typename T>
+    void Scene::OnComponentAdded(Entity entity, T& component)
+    {
+        static_assert(false);
+    }
+
+    template<>
+    void Scene::OnComponentAdded<TransformComponent>(Entity entity, TransformComponent& component)
+    {
+    }
+
+    template<>
+    void Scene::OnComponentAdded<CameraComponent>(Entity entity, CameraComponent& component)
+    {
+        component.Camera.SetViewportSize(m_ViewportWidth, m_ViewportHeight);
+    }
+
+    template<>
+    void Scene::OnComponentAdded<SpriteRendererComponent>(Entity entity, SpriteRendererComponent& component)
+    {
+    }
+
+    template<>
+    void Scene::OnComponentAdded<TagComponent>(Entity entity, TagComponent& component)
+    {
+    }
+
+    template<>
+    void Scene::OnComponentAdded<NativeScriptComponent>(Entity entity, NativeScriptComponent& component)
+    {
+    }
+
 }
