11c4ac9c0df1e8a85fa55d2cfdd1de38ef84a5c8
Wcarey7 <wadecarey7@gmail.com>
2023-07-11 11:49:04 -0700

    Prevent crash when user adds a component that is already present

--- Tulip-Editor/src/Panels/SceneHierarchyPanel.cpp
+++ Tulip-Editor/src/Panels/SceneHierarchyPanel.cpp
@@ -223,13 +223,19 @@ namespace Tulip
         {
             if (ImGui::MenuItem("Camera"))
             {
-                m_SelectionContext.AddComponent<CameraComponent>();
+                if (!m_SelectionContext.HasComponent<CameraComponent>())
+                    m_SelectionContext.AddComponent<CameraComponent>();
+                else
+                    TULIP_CORE_WARN("This entity already has the Camera Component!");
                 ImGui::CloseCurrentPopup();
             }
 
             if (ImGui::MenuItem("Sprite Renderer"))
             {
-                m_SelectionContext.AddComponent<SpriteRendererComponent>();
+                if (!m_SelectionContext.HasComponent<SpriteRendererComponent>())
+                    m_SelectionContext.AddComponent<SpriteRendererComponent>();
+                else
+                    TULIP_CORE_WARN("This entity already has the Sprite Renderer Component!");
                 ImGui::CloseCurrentPopup();
             }
 
