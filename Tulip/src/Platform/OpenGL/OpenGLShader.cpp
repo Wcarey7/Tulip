ea24997046aa8b99c793d3f060b7a1eae4471633
Wcarey7 <wadecarey7@gmail.com>
2023-07-14 16:00:14 -0700

    Added a SetFloat2 to the Shader

--- Tulip/src/Platform/OpenGL/OpenGLShader.cpp
+++ Tulip/src/Platform/OpenGL/OpenGLShader.cpp
@@ -207,6 +207,11 @@ namespace Tulip
         UploadUniformFloat(name, value);
     }
 
+    void OpenGLShader::SetFloat2(const std::string& name, const glm::vec2& value)
+    {
+        UploadUniformFloat2(name, value);
+    }
+
     void OpenGLShader::SetFloat3(const std::string& name, const glm::vec3& value)
     {
         UploadUniformFloat3(name, value);
