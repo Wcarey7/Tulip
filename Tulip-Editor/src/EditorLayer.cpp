416a497fe6a1d9b22522dd0cbc4e4931261a7a40
Wcarey7 <wadecarey7@gmail.com>
2023-07-19 10:52:07 -0700

    Fixed editor camera is receiving events during runtime

--- Tulip-Editor/src/EditorLayer.cpp
+++ Tulip-Editor/src/EditorLayer.cpp
@@ -359,7 +359,10 @@ namespace Tulip
     void EditorLayer::OnEvent(Event& e)
     {
         m_CameraController.OnEvent(e);
-        m_EditorCamera.OnEvent(e);
+        if (m_SceneState == SceneState::Edit)
+        {
+            m_EditorCamera.OnEvent(e);
+        }
 
         EventDispatcher dispatcher(e);
         dispatcher.Dispatch<KeyPressedEvent>(TULIP_BIND_EVENT_FN(EditorLayer::OnKeyPressed));
